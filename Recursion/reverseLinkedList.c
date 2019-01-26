9760a9ff831616f2690d6ec8f58a5bb3b4ce3216
Guneet Garg <guneetgarg.2011@gmail.com>
2019-01-26 15:08:08 +0530

    Update reverseLinkedList.c

--- Recursion/reverseLinkedList.c
+++ Recursion/reverseLinkedList.c
@@ -1,8 +1,19 @@
-#include <stdio.h>
-
 typedef struct Node {
   int data;
   struct Node * next;
 }Node;
 
+Node* reverse(Node* first) {
+  Node* second = NULL;
+  if(first != NULL) {
+    second = first->next;
+  }
+  if(second == NULL) {
+    return first;
+  }
+  Node* curr = reverse(second);
+  first->next = second->next;
+  second->next = first;
+  return curr;
+}
 
